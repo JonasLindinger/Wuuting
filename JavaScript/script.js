73fe67e0bd23a661412ea13a1b2954add95c1efa
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2024-11-10 12:07:16 +0100

    Navbar finished. and commented

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,14 +1,19 @@
+// Button und Menü für Mobilansicht
 const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
 
 toggleBtn.onclick = function () {
+    // Dropdown-Menü öffnen/schließen
     dropDownMenu.classList.toggle("open");
     const isOpen = dropDownMenu.classList.contains("open");
+
+    // Icon anpassen (mit turnary operator (geht anscheinend in javescript))
     toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
 
 window.addEventListener("resize", function () {
+    // Menü schließen bei größerem Bildschirm
     if (window.innerWidth > 992) {
         dropDownMenu.classList.remove("open");
         toggleBtnIcon.classList = "fa-solid fa-bars";
