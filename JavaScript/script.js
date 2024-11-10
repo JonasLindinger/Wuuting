b1836a4fa5958f9a0598153271efb7ea95473216
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2024-11-10 12:00:49 +0100

    Navbar finished.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,16 @@
 const toggleBtn = document.querySelector(".toggle_btn");
-const toggleBtnIcon = document.querySelector(".toggle_btn i")
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
-// Wenn der Button gedrückt wird, öffnen/schließen wir die dropdown navigation
-toggleBtn.onclick = function() {
+
+toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle("open");
-
-    // Icon updaten
     const isOpen = dropDownMenu.classList.contains("open");
-
-    // Turnery Operator gibt es anscheinend auch in JavaScript
     toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-}
+};
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 992) {
+        dropDownMenu.classList.remove("open");
+        toggleBtnIcon.classList = "fa-solid fa-bars";
+    }
+});
