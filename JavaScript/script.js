c5d2e43f09ae3f06b79014204966c700aac2983f
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2024-11-16 21:18:03 +0100

    Working Searchbar (only visible on Product pages)

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,7 @@ const toggleBtn = document.querySelector(".toggle_btn");
 const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".dropdown_menu");
 
+// Öffnet/ Schließt das Menü
 toggleBtn.onclick = function () {
     // Dropdown-Menü öffnen/schließen
     dropDownMenu.classList.toggle("open");
@@ -12,21 +13,46 @@ toggleBtn.onclick = function () {
     toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
 
+// Wenn das Fenster klein ist und das Menü geöffnet ist. Es dann wieder größer wird, schleißt sich das Menü wieder.
 window.addEventListener("resize", function () {
     // Menü schließen bei größerem Bildschirm
-    if (window.innerWidth > 992) {
+    if (window.innerWidth > 950) {
         dropDownMenu.classList.remove("open");
         toggleBtnIcon.classList = "fa-solid fa-bars";
     }
 });
 
-// Suchleiste
-const products = document.querySelectorAll(".product");
-const searchbar = document.querySelector(".searchbar");
+// Das hier läuft nachdem HTML fertig geladen hat.
+document.addEventListener("DOMContentLoaded", () => {
+    // Suchleiste
+    const searchbar = document.querySelector(".searchbar input");
 
-if (products != null) {
-    for (var i = 0; i < products.length; i++) {
-        const productName = products[i].getElementsByClassName("name").item(0).textContent;
-        console.log(productName);
+    const productElements = document.querySelectorAll(".product");
+    let products = {};
+
+    if (productElements.length > 0) {
+        for (var i = 0; i < productElements.length; i++) {
+            const productElement = productElements[i];
+            const productName = productElement.getElementsByClassName("name").item(0).textContent;
+            products[productName] = productElement;
+        }
     }
-}
+
+    // Event listener, für den Fall das die Searchbar benutzt wird.
+    searchbar.addEventListener("input", function (event) {
+        const productNames = Object.keys(products);
+        for (var i = 0; i < productNames.length; i++) {
+            var name = productNames[i];
+            var product = products[name];
+
+            let shouldBeVisible = name.toLowerCase().includes(searchbar.value.toLowerCase());
+
+            if (shouldBeVisible) {
+                product.style="";
+            }
+            else {
+                product.style="display:none";
+            }
+        }
+    });
+});
